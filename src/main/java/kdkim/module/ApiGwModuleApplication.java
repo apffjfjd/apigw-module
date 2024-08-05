765226d4162cc166ebf7f7a5096681085343aecb
apffjfjd <115337453+apffjfjd@users.noreply.github.com>
2024-08-05 18:04:25 +0900

    setting-ing

---
+++ src/main/java/kdkim/module/ApiGwModuleApplication.java
@@ -0,0 +1,13 @@
+package kdkim.module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGwModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGwModuleApplication.class, args);
+    }
+
+}
